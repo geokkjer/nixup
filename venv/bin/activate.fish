72fd20bcd06d4768de1b79041916397221091cb0
Geir Okkenhaug Jerstad <geokkjer@gmail.com>
2022-08-05 11:38:08 +0200

    first build

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/geir/nixosdotfiles/scripts/nixup/venv"
+set -gx VIRTUAL_ENV "/home/geir/code/python/nixup/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
